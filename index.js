29fab6a6da252d9639f17883005477db791da083
Tanya Beaulieu <parkcitycloset@yahoo.com>
2018-02-05 23:26:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,6 +25,6 @@ function currentLine (katzDeliLine) {
       j = i + 1
       line = (line + j + ". " + katzDeliLine[i] + ", ")
     }
-    return (`The line is currently: ${slice(line}`)
+    return (`The line is currently: ${line.slice(-2)}`)
   }
 }
